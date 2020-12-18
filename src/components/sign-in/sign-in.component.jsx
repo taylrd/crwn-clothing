3ed708a575d276a5c8deba15a16ae3d46a75c814
Corey Taylor <corey@taylrd.com>
2020-12-18 15:31:17 -0500

    implemented firebase utils, incduding ability to authenticate users into our firestore database

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -52,7 +52,7 @@ class SignIn extends React.Component {
             required />
             <div className='buttons'>
               <CustomButton type="submit"> Sign In</CustomButton>
-              <CustomButton onClick={signInWithGoogle} isGoogleSignIn>Sign In With Google</CustomButton>
+              <CustomButton onClick={signInWithGoogle} isgooglesignin>Sign In With Google</CustomButton>
             </div>
 
         </form>
--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -11,6 +11,33 @@ const config = {
   appId: '1:707551559442:web:e4b329e857d5921a33fdcb',
 };
 
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+  if (!userAuth) return;
+
+  const userRef = firestore.doc(`users/${userAuth.uid}`);
+
+  const snapShot = await userRef.get();
+  // console.log(snapShot);
+
+  if (!snapShot.exists) {
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+
+    try {
+      await userRef.set({
+        displayName,
+        email,
+        createdAt,
+        ...additionalData,
+      });
+    } catch (error) {
+      console.log('error creating user', error.message);
+    }
+  }
+
+  return userRef;
+};
+
 firebase.initializeApp(config);
 
 export const auth = firebase.auth();
